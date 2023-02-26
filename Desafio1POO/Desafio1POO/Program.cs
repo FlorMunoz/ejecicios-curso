486a7cdf4f008bdecb6ca8e99a8090a56facf6b9
Flor Muñoz <florenciamlombardi@gmail.com>
2023-02-26 12:14:20 -0300

    primeraa commit

---
+++ Desafio1POO/Desafio1POO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1POO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono t1 = new Telefono();
+            t1.Marca = "samsung";
+            t1.Modelo = "s11";
+            t1.llamar();
+
+        
+        }   
+
+    }
+}
---
+++ Desafio1POO/Desafio1POO/Telefono.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1POO
+{
+    public class Telefono
+    {
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+
+        public string NumeroTelefonico { get; set; }
+
+        public int CodigoOperador
+        {
+            get { return  CodigoOperador; }
+
+            set {
+                if (CodigoOperador == 1)
+                {
+                    CodigoOperador = CodigoOperador;
+                }
+                else if (CodigoOperador == 2)
+                {
+                    CodigoOperador = CodigoOperador;
+                }
+                else if (CodigoOperador == 3)
+                {
+                    CodigoOperador = CodigoOperador;
+                }
+                else
+                {
+                    CodigoOperador = 0;
+                }
+            }
+        }
+        public Telefono (string Modelo, string Marca)
+        { this.Modelo = Modelo; this.Marca = Marca; }
+
+        public Telefono()
+        {
+        }
+
+        public void llamar ()
+        {
+            Console.WriteLine("Realizando llamada...");
+        }
+        public void llamar (string contacto)
+        {
+            Console.WriteLine("Realizando llamada..." + contacto);
+        }
+    }
+}
